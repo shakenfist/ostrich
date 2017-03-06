7540787ba886ad7a8e43452c299bfa55effc9303
Michael Still <mikal@stillhq.com>
2017-03-06 11:44:51 +1100

    Refactor steps and emitters

---
+++ emitters.py
@@ -0,0 +1,64 @@
+class NoopEmitter(object):
+    def __init__(self, output):
+        self.output = output
+
+    def clear(self):
+	pass
+
+    def logger(self, logfile):
+        pass
+
+    def emit(self, s):
+        pass
+
+    def getstr(self, s):
+        return None
+
+
+class Emitter(NoopEmitter):
+    def __init__(self, output):
+        self.output = output
+        self.logfile = None
+
+    def clear(self):
+	self.output.clear()
+
+    def logger(self, logfile):
+        if self.logfile:
+            self.logfile.close()
+        self.logfile = open(os.path.expanduser('~/.%s/%s'
+                                               % (progname, logfile)), 'w')
+
+    def emit(self, s):
+        height, width = self.output.getmaxyx()
+
+        for line in s.split('\n'):
+            line = ''.join([i if ord(i) < 128 else ' ' for i in line])
+
+            if self.logfile:
+                self.logfile.write('%s %s\n' %(datetime.datetime.now(), line))
+                self.logfile.flush()
+
+            for l in textwrap.wrap(line, width - 3):
+                if len(l) > 0:
+                    self.output.scroll()
+                    self.output.addstr(height - 2, 1, ' ' * (width - 1))
+
+                    try:
+                        self.output.addstr(height - 2, 2, l)
+                    except Exception as e:
+                        print 'Exception: %s' % e
+                        print '>>%s<<' % line
+                        sys.exit(1)
+
+        self.output.border()
+        self.output.refresh()
+
+    def getstr(self, s):
+        height, width = self.output.getmaxyx()
+
+        self.emit(s)
+        curses.echo()
+        answer = self.output.getstr(height - 2, len(s) + 2)
+        curses.noecho()
+        return answer
---
+++ steps.py
@@ -0,0 +1,128 @@
+class Step(object):
+    def __init__(self, name, depends=None, max_attempts=5):
+        self.name = name
+        self.depends = depends
+        self.attempts = 0
+        self.max_attempts = max_attempts
+
+
+class SimpleCommandStep(Step):
+    def __init__(self, name, command, depends=None, cwd=None, env=None,
+                 max_attempts=5):
+        super(SimpleCommandStep, self).__init__(name, depends=depends,
+                                                max_attempts=max_attempts)
+        self.command = command
+        self.cwd = cwd
+
+        self.env = os.environ
+        self.env.update(env)
+
+    def __str__(self):
+        return 'step %s, depends on %s' %(self.name, self.depends)
+
+    def run(self, emit, screen):
+        emit.emit('Running %s' % self)
+        emit.emit('# %s\n' % self.command)
+        self.attempts += 1
+
+        if self.attempts > self.max_attempts:
+            emit.emit('... repeatedly failed step, giving up')
+            sys.exit(1)
+
+        obj = subprocess.Popen(self.command,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=True,
+                               cwd=self.cwd,
+                               env=self.env)
+        proc = psutil.Process(obj.pid)
+        procs = {}
+
+        flags = fcntl.fcntl(obj.stdout, fcntl.F_GETFL)
+        fcntl.fcntl(obj.stdout, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
+        flags = fcntl.fcntl(obj.stderr, fcntl.F_GETFL)
+        fcntl.fcntl(obj.stderr, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
+        obj.stdin.close()
+        while obj.poll() is None:
+            readable, _, _ = select.select([obj.stderr, obj.stdout], [], [], 0)
+            for f in readable:
+                emit.emit(os.read(f.fileno(), 10000))
+
+            seen = []
+            for child in proc.children(recursive=True):
+                try:
+                    seen.append(child.pid)
+                    if not child.pid in procs:
+                        procs[child.pid] = ' '.join(child.cmdline())
+                        emit.emit('*** process started *** %d -> %s'
+                                  %(child.pid, procs[child.pid]))
+                except psutil.NoSuchProcess:
+                    pass
+
+            ended = []
+            for pid in procs:
+                if not pid in seen:
+                    emit.emit('*** process ended *** %d -> %s'
+                              %(pid, procs.get(child.pid, '???')))
+                    ended.append(pid)
+
+            for pid in ended:
+                del procs[pid]
+
+        emit.emit('... process complete')
+        returncode = obj.returncode
+        emit.emit('... exit code %d' % returncode)
+        return returncode == 0
+
+
+class QuestionStep(Step):
+    def __init__(self, name, title, helpful, prompt, depends=None):
+        super(QuestionStep, self).__init__(name, depends)
+        self.title = title
+        self.help = helpful
+        self.prompt = prompt
+
+    def run(self, emit, screen):
+        emit.emit('%s' % self.title)
+        emit.emit('%s\n' % ('=' * len(self.title)))
+        emit.emit('%s\n' % self.help)
+        return emit.getstr('>> ')
+
+
+class RegexpEditorStep(Step):
+    def __init__(self, name, path, search, replace, cwd=None, env=None):
+        super(RegexpEditorStep, self).__init__(name)
+        self.path = path
+        if cwd:
+            self.path = os.path.join(cwd, path)
+
+        self.search = search
+        self.replace = replace
+
+    def run(self, emit, screen):
+        output = []
+        changes = 0
+
+        emit.emit('--- %s' % self.path)
+        emit.emit('+++ %s' % self.path)
+
+        with open(self.path, 'r') as f:
+            for line in f.readlines():
+                line = line.rstrip()
+                newline = re.sub(self.search, self.replace, line)
+                output.append(newline)
+
+                if newline != line:
+                    emit.emit('- %s' % line)
+                    emit.emit('+ %s' % newline)
+                    changes += 1
+                else:
+                    emit.emit('  %s' % line)
+
+        with open(self.path, 'w') as f:
+            f.write('\n'.join(output))
+
+        return 'Changed %d lines' % changes
