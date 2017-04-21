510e1b6747a73cd27116796da4ed4bf60bc88059
Michael Still <mikal@stillhq.com>
2017-04-21 14:01:40 +1000

    We're truncating reads for shortlived processes

--- ostrich/steps.py
+++ ostrich/steps.py
@@ -141,6 +141,13 @@ class SimpleCommandStep(Step):
             for pid in ended:
                 del procs[pid]
 
+        # One final read pass
+        readable, _, _ = select.select([obj.stderr, obj.stdout], [], [], 1)
+        for f in readable:
+            d = os.read(f.fileno(), 10000)
+            self._output_analysis(d)
+            emit.emit(d)
+
         emit.emit('... process complete')
         returncode = obj.returncode
         emit.emit('... exit code %d' % returncode)
